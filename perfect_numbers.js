7b75059c582afd03566ba481e25bbe5aab27ddfe
optzap-m@mail.ru <mihail@DESKTOP-I1GGCGG.localdomain>
2026-02-11 17:38:11 +0500

    cовершенные числа

---
+++ perfect_numbers.js
@@ -0,0 +1,27 @@
+const sumNum = (number) => {
+    let i = 1 
+    let sum = 0
+    while (i <= number/2) {
+        if (number % i === 0){
+            sum = sum + i
+        }
+        else{
+            sum = sum + 0
+        }
+        i++
+    }
+    return sum
+}
+
+const isPerfect = (number) => {
+    let result = sumNum(number)
+    if (number === result) {
+        return true
+    } 
+    else {
+        return false
+    }
+}
+
+console.log(isPerfect (28))
+console.log(sumNum(496))

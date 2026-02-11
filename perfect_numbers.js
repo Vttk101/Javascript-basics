98e8dd43e0b89f6f7b914c476502f016f2cd506a
optzap-m@mail.ru <mihail@DESKTOP-I1GGCGG.localdomain>
2026-02-11 17:55:36 +0500

    update2 perfect_numbers

--- perfect_numbers.js
+++ perfect_numbers.js
@@ -28,3 +28,21 @@ const isPerfect = (number) => {
 
 console.log(isPerfect (28))
 console.log(sumNum(496))
+
+//Решение учителя:
+const isPerfect = (num) => {
+  if (num < 6) {
+    return false
+  }
+
+  let sum = 0
+  const upperBorder = num / 2
+
+  for (let divisor = 1; divisor <= upperBorder; divisor += 1) {
+    if (num % divisor === 0) {
+      sum += divisor
+    }
+  }
+
+  return sum === num
+}
